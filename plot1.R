ee8c3f0b51abef65604acfa5c3f6b354779bf205
Thomas MacPherson <tmacpherson6@gmail.com>
2024-02-14 10:35:18 -0700

    Finalized hist code

--- plot1.R
+++ plot1.R
@@ -1,14 +1,13 @@
 ## Global active power histogram Base Graphics
 
-# Realistically, this simple hist can be called in a single line
-# I have broken into a few just for an example 
-
 # Open Graphics Device (PNG File)
 png(filename="plot1.png",width=480,height=480,units="px")
 
 # Construct simple histogram
-hist(data$global_active_power,col="red")
-title(main="Global Active Power",xlab="Global Active Power (kilowatts)")
+hist(data$global_active_power,col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
 
 # Close Graphics Device
 dev.off()
