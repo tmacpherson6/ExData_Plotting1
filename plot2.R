501f1f29dcadb37dc9a791647ee8286c9bd72ce6
Thomas MacPherson <tmacpherson6@gmail.com>
2024-02-14 13:39:37 -0700

    Completed plot 2

--- plot2.R
+++ plot2.R
@@ -1,6 +1,14 @@
 ## Plot 2 - Global Active Power vs time.
 
+# Open Graphic Device
+PNG(filename="plot2.png",width=480,height=480,units="px")
+
+# Create line graph
 with(data,plot(datetime,global_active_power,
      type="l",xlab=" ",ylab="Global Active Power (kilowatts)",xaxt="n"))
-axis(1,at=c(-4,1,0),
-     labels=c("Thu","Fri","Sat"))
+x<-ymd_hms("2007-02-01 00:00:00","2007-02-02 00:00:00","2007-02-02 23:59:00")
+x<-as.numeric(x)
+axis(1,at=x,labels=c("Thu","Fri","Sat"))
+
+# Close Graphic Device
+dev.off()
